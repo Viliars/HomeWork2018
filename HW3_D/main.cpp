048d4b18032770497920964f2e7b8aca29055d55
Viliars <redkillerpw@mail.ru>
2018-03-17 20:36:01 +0300

    Algorithm was changed

--- HW3_D/main.cpp
+++ HW3_D/main.cpp
@@ -1,68 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int getMax(unsigned int arr[], unsigned int n)
+void BitsRadixSort(unsigned int a[], int n)
 {
-    int mx = arr[0];
-    for (unsigned int i = 1; i < n; i++)
-        if (arr[i] > mx)
-            mx = arr[i];
-    return mx;
-}
+    //  255 = 11111111
+    unsigned bufer[n];
+    int buf[256];
+    int point[256];
+    for (int i = 0; i < 32; i +=8)
+	{
+		point[0] = 0;
 
-void countSort(unsigned int arr[], unsigned int n, int exp)
-{
-    int output[n];
-    int i, count[10] = {0};
+		// buf=0
+		for (int j = 0; j < 256; j++) buf[j] = 0;
 
-    for (i = 0; i < n; i++)
-        count[ (arr[i]/exp)%10 ]++;
-
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    for (i = n - 1; i >= 0; i--)
-    {
-        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
-        count[ (arr[i]/exp)%10 ]--;
-    }
-
-    for (i = 0; i < n; i++)
-        arr[i] = output[i];
-}
-
-
-void radixsort(unsigned int arr[], int n)
-{
-
-    int m = getMax(arr, n);
-    for (int exp = 1; m/exp > 0; exp *= 10)
-        countSort(arr, n, exp);
+		for (int j = 0; j < n; j++) buf[a[j] >> i & 255]++;
+		for (int j = 1; j < 256; j++) point[j] = point[j - 1] + buf[j - 1];
+		for (int j = 0; j < n; ++j) bufer[point[a[j] >> i & 255]++] = a[j];
+		for(int i=0;i<n;i++) a[i]=bufer[i];
+	}
 }
 
 int main()
 {
-unsigned int n,k,m,l;
+unsigned n,k,m,l;
 cin>>n>>k>>m>>l;
-unsigned int a[n];
+unsigned a[n];
+// ------------------------------------------------------------------------------------------------------------
 a[0] = k;
-for (unsigned int i = 0; i < n-1; i++)
-a[i+1] = (unsigned int)((a[i]*(unsigned long long)m)&0xFFFFFFFFU)%l;
-radixsort(a,n);
+for (unsigned int i = 0; i < n-1; i++) a[i+1] = (unsigned int)((a[i]*(unsigned long long)m)&0xFFFFFFFFU)%l;
+// ------------------------------------------------------------------------------------------------------------
 unsigned long long sum=0;
-unsigned half=n/2;
-for(unsigned int i=0;i<n;i++)
-{
-    cout<<a[i]<<" ";
-}
-cout<<endl;
-for(unsigned int i=0;i<n;i+=2)
-{
-    sum+=a[i];
-    if(i==half) sum%=l;
-}
 
-cout<<sum%l;
+BitsRadixSort(a,n);
+
+for (int i = 0; i < n; i += 2)
+{
+    sum = (sum + a[i]) % l;
+}
+cout << sum;
 
     return 0;
 }
