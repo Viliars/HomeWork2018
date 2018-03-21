b4ae5154a0a3b411f6ae193cb51cd44ad4f89989
Viliars <redkillerpw@mail.ru>
2018-03-21 08:21:28 +0300

    Last update

--- HW3_D/main.cpp
+++ HW3_D/main.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-// RADIX
-void BitsRadixSort(unsigned int a[], int n)
-{
-    //  255 = 11111111
-    unsigned bufer[n];
-    int buf[256];
-    int point[256];
-    for (int i = 0; i < 32; i +=8)
-	{
-	    // vector for sum
-		point[0] = 0;
-		// buf=0
-		for (int k = 0; k < 256; k++) buf[k] = 0;
-        // count
-		for (int k = 0; k < n; k++) buf[a[k] >> i & 255]++;
-        // run sum
-		for (int k = 1; k < 256; k++) point[k] = point[k - 1] + buf[k - 1];
-        // set locate
-		for (int k = 0; k < n; k++) bufer[point[a[k] >> i & 255]++] = a[k];
-
-		// swap
-		for(int i=0;i<n;i++) a[i]=bufer[i];
-	}
-}
-
+const unsigned FF=256;
+int buf[FF];
 int main()
 {
-unsigned n,k,m,l;
+int n;
+unsigned k,m,l;
+const unsigned FF=256;
+const unsigned eight=8;
 cin>>n>>k>>m>>l;
-unsigned a[n];
-// ------------------------------------------------------------------------------------------------------------
+vector<unsigned> a(n);
+vector<unsigned> bufer(n);
+int point[FF];
 a[0] = k;
-for (unsigned int i = 0; i < n-1; i++) a[i+1] = (unsigned int)((a[i]*(unsigned long long)m)&0xFFFFFFFFU)%l;
-// ------------------------------------------------------------------------------------------------------------
+for (int i = 1; i < n; ++i)
+	{
+		a[i] = (unsigned int) ((a[i - 1] * (unsigned long long) m) & 0xFFFFFFFFU) % l;
+	}
 unsigned long long sum=0;
-
-BitsRadixSort(a,n);
-
-for (int i = 0; i < n; i += 2)
+for (int i = 0; i < 32; i+=eight)
 {
-    sum = (sum + a[i]) % l;
+    point[0] = 0;
+    for (int k = 0; k < FF; ++k) buf[k] = 0;
+    for (int k = 0; k < n; ++k) ++buf[a[k] >> i & (FF-1)];
+    for (int k = 1; k < FF; ++k) point[k] = point[k - 1] + buf[k - 1];
+    for (int k = 0; k < n; ++k) bufer[point[a[k] >> i & (FF-1)]++] = a[k];
+    a.swap(bufer);
 }
-cout << sum;
+
+for (int i = 0; i < n; i+=2)
+{
+    sum+=a[i];
+}
+sum%=l;
+cout<<sum;
     return 0;
 }
