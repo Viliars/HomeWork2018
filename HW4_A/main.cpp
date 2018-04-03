2003bbea38fd75078294133a05a6a7f04b5224cb
Viliars <redkillerpw@mail.ru>
2018-04-03 19:41:46 +0300

    Update HW4

---
+++ HW4_A/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+int n,k;
+cin>>n>>k;
+vector<int> a(n);
+for(int i=0;i<n;++i)
+{
+    cin>>a[i];
+}
+sort(a.begin(),a.end());
+int max=a[n-1];
+int lf=0;
+int rg=max;
+int sum=0;
+while(lf+1<rg)
+{
+    int mid=(lf+rg)>>1;
+    sum=0;
+    for(int i=0;i<n;++i) sum+=a[i]%mid;
+    if(sum>=k) rg=mid;
+    else lf=mid;
+}
+sum=0;
+for(int i=0;i<n;++i) sum+=a[i]%rg;
+if(sum>=k) cout<<rg;
+else cout<<lf;
+    return 0;
+}
