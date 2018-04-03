36536012b63ec1df86cde344b676a6973aad009a
Viliars <redkillerpw@mail.ru>
2018-04-03 13:03:21 +0300

    03.04.2018

--- HW3_E/main.c
+++ HW3_E/main.c
@@ -1,36 +1,76 @@
+/* Author: Michael Pritugin 241 gr
+Development was cunducted from 28.10.2017 to 29.10.2017
+Program "sort" betta version
+
+Possible parameters:
++n: sort since n of a line
+-m: merge all files
+-o: using output file
+-r: upside-down
+
+Possible mistakes:
+#3: error opening of the file
+#2: error of closing of the file
+#1: file deletion error */
+/* MODE`s bytes */
+#define PAR_R (1<<3)
+#define PAR_N (1<<2)
+#define PAR_M (1<<1)
+#define PAR_O (1)
+/* MODE`s mask */
+#define MOD_R(a) (((a)>>3)&1)
+#define MOD_N(a) (((a)>>2)&1)
+#define MOD_M(a) (((a)>>1)&1)
+#define MOD_O(a) ((a)&1)
+
+/* debuf setting */
+#define NO_DEBUG
+//--------------------------------------------------
+/* The program is changed to the task of the AaDS */
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+//#include "sort.h"
+#define too(a) (1<<a)
+#define last(b) (strlen(b)-1)
+#define bit(c) (c&1)
+
 
 
 // int fputs( const char * string, FILE * filestream );
 // char * fgets( char * string, int num, FILE * filestream );
 // int strcmp( const char * string1, const char * string2 );
-
-int too(int core)
-{
- // 2^core
-    return 1<<core;
-}
+// FILE * fopen( const char * fname, const char * modeopen );
+// int fclose( FILE * filestream );
+// int remove( const char * fname );
 
 void CopyFile(const char* name_input,const char* name_output,char** string,int counter)
 {
+#ifdef DEBUG
+    printf("In function CopyFile\n");
+#endif // DEBUG
     FILE* IN = fopen(name_input, "r");
 	FILE* OUT = fopen(name_output, "w");
 	for (int i = 1; i < counter; i++)
 	{
-		fgets(string[0], 10010, IN);
+		fgets(string[0], 10011, IN);
 		fputs(string[0], OUT);
 	}
-	fgets(string[0], 10010, IN);
-	string[0][strlen(string[0]) - 1] = '\0';
+	fgets(string[0], 10011, IN);
+	string[0][last(string[0])] = '\0';
 	fputs(string[0], OUT);
 	fclose(OUT);
 	fclose(IN);
+#ifdef DEBUG
+    printf("End function CopyFile\n");
+#endif // DEBUG
 }
 
 void external_sorting(const char* name_input,const char* name_output)
 {
+#ifdef DEBUG
+    printf("In function external_sorting\n");
+#endif // DEBUG
     int core;
     int i=0, j=0;
     int n=0, k=0;
@@ -51,21 +91,31 @@ void external_sorting(const char* name_input,const char* name_output)
     }
     //-----------------------------------------------
 	int choose;
-	while (fgets(string[0], 10010, IN)) { fputs(string[0], bufer[counter&1]); counter++; }
-	fputc('\n', bufer[!(counter & 1)]);
+#ifdef DEBUG
+    printf("After malloc and before Main While\n");
+#endif // DEBUG
+	while (fgets(string[0], 10011, IN)) { fputs(string[0], bufer[bit(counter)]); counter++; }
+	fputc('\n', bufer[!bit(counter)]);
 	fclose(IN);
 	fclose(bufer[0]); fclose(bufer[1]);
 	bufer[0] = fopen("bufer_0_241.txt", "r"); bufer[1] = fopen("bufer_1_241.txt", "r");
+#ifdef DEBUG
+    printf("After Main While before Main FOR\n");
+#endif // DEBUG
 	for (core = 0; (counter - 1) >> core > 0; core++)
 	{
 		n = too(core);
 		k = counter >> (core + 1);
 		for (center = 0; center < k; center++)
 		{
-		    fgets(string[0], 10010, bufer[0]);
-            fgets(string[1], 10010, bufer[1]);
+		    fgets(string[0], 10011, bufer[0]);
+            fgets(string[1], 10011, bufer[1]);
             i = 0;
             j = 0;
+// --------------------------------------- GIPERION -------------------------------------------
+#ifdef DEBUG
+    printf("Label GIPERION\n");
+#endif // DEBUG
 			for (; i < n && j < n;)
 			{
 				choose = strcmp(string[0], string[1]);
@@ -73,13 +123,13 @@ void external_sorting(const char* name_input,const char* name_output)
 				{
 					fputs(string[0], OUT);
 					i++;
-					if (i < n) fgets(string[0], 10010, bufer[0]);
+					if (i < n) fgets(string[0], 10011, bufer[0]);
 				}
 				else
 				{
 					fputs(string[1], OUT);
 					j++;
-					if (j < n) fgets(string[1], 10010, bufer[1]);
+					if (j < n) fgets(string[1], 10011, bufer[1]);
 				}
 			}
 			if (i < n)
@@ -88,7 +138,7 @@ void external_sorting(const char* name_input,const char* name_output)
 				i++;
 				for (; i < n; i++)
 				{
-					fgets(string[0], 10010, bufer[0]);
+					fgets(string[0], 10011, bufer[0]);
 					fputs(string[0], OUT);
 				}
 			}
@@ -98,18 +148,22 @@ void external_sorting(const char* name_input,const char* name_output)
 				j++;
 				for (; j < n; j++)
 				{
-					fgets(string[1], 10010, bufer[1]);
+					fgets(string[1], 10011, bufer[1]);
 					fputs(string[1], OUT);
 				}
 			}
 		}
+// ------------------------------------------------- KRIOS ---------------------------------------------------
+#ifdef DEBUG
+    printf("Label KRIOS\n");
+#endif // DEBUG
 		k = counter & (too(core) - 1);
 		center = counter & too(core);
 
 		if (center && k)
 		{
-		    fgets(string[0], 10010, bufer[0]);
-            fgets(string[1], 10010, bufer[1]);
+		    fgets(string[0], 10011, bufer[0]);
+            fgets(string[1], 10011, bufer[1]);
             i = 0;
             j = 0;
 			for (; i < n && j < k;)
@@ -119,13 +173,13 @@ void external_sorting(const char* name_input,const char* name_output)
 				{
 					fputs(string[0], OUT);
 					i++;
-					if (i < n) fgets(string[0], 10010, bufer[0]);
+					if (i < n) fgets(string[0], 10011, bufer[0]);
 				}
 				else
 				{
 					fputs(string[1], OUT);
 					j++;
-					if (j < k) fgets(string[1], 10010, bufer[1]);
+					if (j < k) fgets(string[1], 10011, bufer[1]);
 				}
 			}
 			if (i < n)
@@ -133,7 +187,7 @@ void external_sorting(const char* name_input,const char* name_output)
 				fputs(string[0], OUT);
 				for (i++; i < n; i++)
 				{
-					fgets(string[0], 10010, bufer[0]);
+					fgets(string[0], 10011, bufer[0]);
 					fputs(string[0], OUT);
 				}
 			}
@@ -143,35 +197,47 @@ void external_sorting(const char* name_input,const char* name_output)
 				j++;
 				for (; j < k; j++)
 				{
-					fgets(string[1], 10010, bufer[1]);
+					fgets(string[1], 10011, bufer[1]);
 					fputs(string[1], OUT);
 				}
 			}
 		}
+// -------------------------------------------- OKEAN --------------------------------------
 		else
 		{
+#ifdef DEBUG
+    printf("Label OKEAN\n");
+#endif // DEBUG
 		    center+=k;
 			for (i = 0; i < center; i++)
 			{
-				fgets(string[0], 10010, bufer[0]);
+				fgets(string[0], 10011, bufer[0]);
 				fputs(string[0], OUT);
 			}
 		}
 		fclose(OUT);
 		fclose(bufer[0]);
 		fclose(bufer[1]);
+#ifdef DEBUG
+    printf("before BREAK\n");
+#endif // DEBUG
 		if ((counter - 1) >> core == 1) break;
 		OUT = fopen("bufer_2_241.txt", "r");
 		bufer[0] = fopen("bufer_0_241.txt", "w");
 		bufer[1] = fopen("bufer_1_241.txt", "w");
-		k = ((counter - 1) / (n <<= 1)) + 1;
+		n<<=1;
+		k = ((counter - 1) / n) + 1;
+#ifdef DEBUG
+    printf("Label TITAN \n");
+#endif // DEBUG
+//--------------------------------------------------- TITAN -----------------------------------------------------------
 		for (center = 0; center < k; center++)
 		{
 			i = 0;
-			while (i < n && fgets(string[0], 10010, OUT))
+			while (i < n && fgets(string[0], 10011, OUT))
 			{
 			    i++;
-				fputs(string[0], bufer[center & 1]);
+				fputs(string[0], bufer[bit(center)]);
 			}
 		}
 		fclose(OUT);
@@ -181,16 +247,25 @@ void external_sorting(const char* name_input,const char* name_output)
 		bufer[0] = fopen("bufer_0_241.txt", "r");
 		bufer[1] = fopen("bufer_1_241.txt", "r");
 	}
+#ifdef DEBUG
+    printf("Before CopyFile\n");
+#endif // DEBUG
 	CopyFile("bufer_2_241.txt",name_output,string,counter);
 	remove("bufer_0_241.txt");
 	remove("bufer_1_241.txt");
 	remove("bufer_2_241.txt");
+#ifdef DEBUG
+    printf("End of function external_sorting\n");
+#endif // DEBUG
 }
 
 
-
+//main
 int main(int argc,char* argv[])
 {
+#ifdef DEBUG
+    printf("Start program\n");
+#endif // DEBUG
     external_sorting("input.txt","output.txt");
 	return 0;
 }
---
+++ HW4_E/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+namespace hull{
+    typedef int uint;
+    const uint FF=256;
+    const uint FE=255;
+    const uint eight=8;
+
+    class Point {
+        public:
+            uint x,y;
+            Point(uint x=0,uint y=0) { this->x=x; this->y=x; }
+            bool operator <(const Point& another)
+            {
+                return (x < another.x) || ((x == another.x) && (y < another.y));
+            }
+             friend std::istream& operator >> ( std::istream& os, Point& obj);
+             friend std::ostream& operator << ( std::ostream& os, Point& obj);
+    };
+
+    std::istream& operator >> ( std::istream& os, Point& obj)
+    {
+    os>>obj.x>>obj.y;
+    return os;
+    }
+
+    std::ostream& operator << ( std::ostream& os, Point& obj)
+    {
+    os<<obj.x<<std::endl<<obj.y;
+    return os;
+    }
+
+    bool ClockWise (const Point& a,const Point& b,const Point& c)
+    {
+	return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+    }
+
+    bool CounterClockWise (const Point& a,const Point& b,const Point& c)
+    {
+        return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+    }
+
+    void convex_hull (std::vector<Point> & a)
+    {
+        if (a.size() == 1)  return;
+        sort(a.begin(),a.end());
+        Point p1 = a[0],  p2 = a.back();
+        std::vector<Point> up, down;
+        up.push_back (p1);
+        down.push_back (p1);
+        for (size_t i=1; i<a.size(); ++i)
+        {
+            if (i==a.size()-1 || ClockWise(p1, a[i], p2))
+            {
+                while (up.size()>=2 && !ClockWise(up[up.size()-2], up[up.size()-1], a[i])) up.pop_back();
+                up.push_back (a[i]);
+            }
+            if (i==a.size()-1 || CounterClockWise(p1, a[i], p2))
+            {
+                while (down.size()>=2 && !CounterClockWise(down[down.size()-2], down[down.size()-1], a[i])) down.pop_back();
+                down.push_back (a[i]);
+            }
+        }
+        a.clear();
+        for (size_t i=0; i<up.size(); ++i) a.push_back (up[i]);
+        for (size_t i=down.size()-2; i>0; --i) a.push_back (down[i]);
+    }
+
+}
+
+using namespace std;
+using namespace hull;
+
+int sqr(int x)
+{
+    return x*x;
+}
+
+int main()
+{
+    vector<Point> a;
+    Point c;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i)
+    {
+        cin>>c;
+        a.push_back(c);
+    }
+    convex_hull(a);
+    double per=0;
+    cout.precision(3);
+    for(size_t i=1;i<a.size();++i)
+    {
+        per+=sqrt(sqr(a[i].x-a[i-1].x)+sqr(a[i].y-a[i-1].y));
+    }
+    double buf=sqrt(sqr(a[a.size()-1].x-a[0].x)+sqr(a[a.size()-1].y-a[0].y));
+    if(per!=buf)
+    {
+     per+=buf;
+    }
+    cout<<per;
+    return 0;
+}
