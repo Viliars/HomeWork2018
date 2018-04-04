4e16fd5484f355524fd144eead6104c5be8395e7
Viliars <redkillerpw@mail.ru>
2018-04-04 15:51:01 +0300

    New

--- HW4_B/main.cpp
+++ HW4_B/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<cstdio>
 #include<vector>
 #include<algorithm>
 
@@ -6,36 +6,39 @@ using namespace std;
 
 int main()
 {
-	int n, m, rez = 0;
+	int n, m;
+	int rez;
 	int core;
 	int buf;
-	cin >> n >> m;
-	vector<vector<int> > a(n);
+	scanf("%d %d",&n,&m);
+	vector< vector<int> > a(n);
 	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 0; j < m; ++j)
+		for (int k = 0; k < m; ++k)
 		{
-			cin >> buf; a[i].push_back(buf);
+			scanf("%d",&buf); a[i].push_back(buf);
 		}
 	}
     // all sort
 	for(int i=0;i<n;++i) sort(a[i].begin(),a[i].end());
 //------------------------------------------------------
+    rez=0;
 	for (int i = 0; i < n; ++i)
 		for (int k = i + 1; k < n; ++k)
 		{
 			core = 0;
 			int j=0;
 			int l=0;
-			for (; j < m && l < m;)
+			while(true)
             {
+                if(j >= m || l >= m) break;
                 if (a[i][j] < a[k][l]) j++;
                 else
                     if(a[i][j] > a[k][l]) l++;
-                    else { core++, j++, l++; }
+                    else { core++; j++; l++; }
             }
 			if (rez < core) rez = core;
 		}
-	cout << rez;
+	printf("%d",rez);
 	return 0;
 }
